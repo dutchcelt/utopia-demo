a0a3269bcd28a036737e5eb66ac3857e906da775
dutchcelt <studio@dutchcelt.com>
2024-02-25 22:09:14 +0100

    Added global breakpoint values

--- transforms/utopia.js
+++ transforms/utopia.js
@@ -1,25 +1,33 @@
-const StyleDictionary = require("style-dictionary");
-const { calculateClamp } = require("utopia-core");
+const path = require('path');
+
+const StyleDictionary = require('style-dictionary');
+const { calculateClamp } = require('utopia-core');
+const { layout } = require(path.resolve('.', 'tokens', 'layout', 'breakpoint.json'));
 
 const isUtopiaCalculateClamp = (token) => {
-  return token?.$extensions?.["utopia.calculate.clamp"];
+	return token?.$extensions?.['utopia.calculate.clamp'];
+};
+
+const scaleBoundries = {
+	minWidth: layout.breakpoint.mobile.value,
+	maxWidth: layout.breakpoint.desktop.value,
 };
 
 StyleDictionary.registerTransform({
-  name: "utopia/calculate/clamp",
-  type: `value`,
-  transitive: true,
-  matcher: isUtopiaCalculateClamp,
-  transformer: (token) => {
-    const config = token.$extensions["utopia.calculate.clamp"];
-    return calculateClamp({ ...config });
-  },
+	name: 'utopia/calculate/clamp',
+	type: `value`,
+	transitive: true,
+	matcher: isUtopiaCalculateClamp,
+	transformer: (token) => {
+		const minMaxSizes = token.$extensions['utopia.calculate.clamp'];
+		return calculateClamp({
+			...scaleBoundries,
+			...minMaxSizes,
+		});
+	},
 });
 
 StyleDictionary.registerTransformGroup({
-  name: "utopia/css",
-  transforms: [
-    ...StyleDictionary.transformGroup["css"],
-    "utopia/calculate/clamp",
-  ],
+	name: 'utopia/css',
+	transforms: [...StyleDictionary.transformGroup['css'], 'utopia/calculate/clamp'],
 });
